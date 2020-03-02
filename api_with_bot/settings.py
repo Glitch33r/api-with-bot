95f38788572ebd586e44ef7396ce4e6e89755aa0
DMK <dmkshift@gmail.com>
2020-03-02 22:32:33 +0200

    static files

--- api_with_bot/settings.py
+++ api_with_bot/settings.py
@@ -143,3 +143,5 @@ STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, "static")
 
 AUTH_USER_MODEL = 'core.User'
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'

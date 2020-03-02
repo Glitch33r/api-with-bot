ffbfe48f547082c41595610fb7a114494d937cd2
DMK <dmkshift@gmail.com>
2020-03-02 22:37:11 +0200

    static files

--- api_with_bot/urls.py
+++ api_with_bot/urls.py
@@ -1,10 +1,12 @@
 from django.contrib import admin
 from django.urls import path, include
 from automate.views import automate_api
+from django.conf.urls.static import static
+from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/sn/user/', include('user.urls')),
     path('api/sn/post/', include('post.urls')),
     path('auto-bot/', automate_api)
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
